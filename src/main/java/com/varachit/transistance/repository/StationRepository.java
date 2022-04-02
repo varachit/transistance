5d9e6e31250f55a18cc93cacaf9ba6754bccfd86
Varachit W <doctors.csgo@gmail.com>
2022-04-02 17:14:33 +0700

    Declared additional SQL Query

--- src/main/java/com/varachit/transistance/repository/StationRepository.java
+++ src/main/java/com/varachit/transistance/repository/StationRepository.java
@@ -9,9 +9,15 @@ import java.util.Optional;
 
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
+    @Query("SELECT s FROM Station s")
     Optional<Station> findByCode(String code);
 
+    @Query("SELECT s FROM Station s WHERE s.name = ?1")
     List<Station> searchStationsByName(String name);
+
+    @Query("SELECT s FROM Station s WHERE s.type = ?1")
     List<Station> searchStationsByType(StationType type);
+
+    @Query("SELECT s FROM Station s WHERE s.name = ?1 AND s.type = ?2")
     List<Station> searchStationsByNameAndType(String name, StationType type);
 }
