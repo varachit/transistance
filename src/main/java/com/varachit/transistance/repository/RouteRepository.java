fa0e88879e147f86de8b694377c27ba5cad9290d
Varachit W <doctors.csgo@gmail.com>
2022-04-02 17:14:27 +0700

    Declared additional SQL Query

--- src/main/java/com/varachit/transistance/repository/RouteRepository.java
+++ src/main/java/com/varachit/transistance/repository/RouteRepository.java
@@ -2,11 +2,13 @@ package com.varachit.transistance.repository;
 
 import com.varachit.transistance.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
+    @Query("SELECT r FROM Route r")
     Optional<Route> findByName(String name);
 }
